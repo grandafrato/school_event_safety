c86886772e3d9d1214a37ddd30a529590ed2e74a
Lachlan Wilger <lachlan@wilgerfamily.com>
2023-05-11 09:48:12 -0700

    Add leptos_router.

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,37 @@
+use crate::event::Event;
 use leptos::*;
+use leptos_router::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     view! { cx,
-        <h1>"Hello, world."</h1>
+        <Router>
+            <Routes>
+                <Route path="/" view=move |cx| {
+                    view! { cx, <IndexView/>}
+                }/>
+                <Route path="/game" view=move |cx| {
+                    view! { cx, <GameView/>}
+                }/>
+            </Routes>
+        </Router>
+        <footer>
+            <p>"Copyright (c) 2023 Lachlan Wilger."</p>
+        </footer>
+    }
+}
+
+#[component]
+fn IndexView(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <h1>"Building an Event, Safely"</h1>
+        <A href="/game">"Start Event"</A>
+    }
+}
+
+#[component]
+fn GameView(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <h1>"Game, it is."</h1>
     }
 }
