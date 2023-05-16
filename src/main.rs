d0e34818fd24e224b35fbb64fe38f12f7ac9b61f
Lachlan Wilger <lachlan@wilgerfamily.com>
2023-05-15 21:40:40 -0700

    Change global allocator to wee_alloc.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use leptos::{mount_to_body, view};
 use school_event_safety::app::App;
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 fn main() {
     mount_to_body(|cx| view! { cx, <App/> })
 }
